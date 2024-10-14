1b570b790aaa8a5bc6a5500debc828974423f07e
Samandar <zafarovsamandar5444@gmail.com>
2024-10-14 14:28:11 +0500

    12345678

---
+++ main.js
@@ -0,0 +1,9 @@
+let olma = +prompt("Olmani narxinini kiriting");
+let uzum = +prompt("Uzumni narxinini kiriting");
+let behi = +prompt("Behini narxinini kiriting");
+let shoftoli = +prompt("Shoftolini narxinini kiriting");
+let banan = +prompt("Banan narxinini kiriting");
+let narx = olma + uzum + behi + shoftoli + banan;
+
+console.log(narx);
+alert("jami narxlar" + narx);
